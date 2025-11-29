3b978c371300cfca640ff1890c28186213559543
dew <grennys210106@gmail.com>
2025-11-29 23:04:57 +0700

    add hash for token, started to create some middlewares

--- utils/hash.go
+++ utils/hash.go
@@ -1,6 +1,11 @@
 package utils
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"os"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -12,6 +17,25 @@ func HashPassword(password string) (hash string, err error) {
 	return string(bytes), nil
 }
 
-func HashSHA256() {
+func HashTokenHMAC(token string) string {
+	var SecretKey = []byte(os.Getenv("SECRET_KEY"))
 
+	mac := hmac.New(sha256.New, SecretKey)
+	mac.Write([]byte(token))
+	return hex.EncodeToString(mac.Sum(nil))
+}
+
+func CompareTokenHash(token, hash string) bool {
+	secretKey := []byte(os.Getenv("SECRET_KEY"))
+
+	mac := hmac.New(sha256.New, secretKey)
+	mac.Write([]byte(token))
+	expectedMAC := mac.Sum(nil)
+
+	hashBytes, err := hex.DecodeString(hash)
+	if err != nil {
+		return false
+	}
+
+	return hmac.Equal(expectedMAC, hashBytes)
 }
